9cbde6e1d4246f0c5fb6fb0a54eb3b9011151e31
Jack Rundle <jrundle@nd.edu>
2020-01-02 15:00:15 -0600

    updates to test

--- spotify.py
+++ spotify.py
@@ -36,6 +36,9 @@ separate program:
 
 
 import requests
+import json
+from os import path
+
 
 BASE = "https://api.spotify.com"
 CLIENT_ID = "3ef02e948f334570b661cbfe781b941a"
@@ -43,32 +46,66 @@ CLIENT_SECRET = "bca8bff649c84dcb8c3dfde7ec0efc4c"
 
 
 class Authentication:
+    cache_file = ".spotify-token"
 
-    def __init__(self, client_id, redirect_uri):
+    def __init__(self, client_id, client_secret, redirect_uri, trace=False):
         self.client_id      = client_id
+        self.client_secret  = client_secret
         self.redirect_uri   = redirect_uri
 
         self.access_token  = None
         self.refresh_token = None
         self.token_expired = True
 
+        self.trace = False
+
+    def _get(self, endpoint, params=None):
+        headers = { 'Content-Type': 'application/json' }
+
+        resp = requests.get(BASE + endpoint, headers=headers, params=params)
+        
+        if self.trace:
+            print("GET", BASE + endpoint)
+            print("Headers:", headers)
+            print("Params:", json.dumps(params, 4))
+            print("Response:", resp.json())
+
+        return resp
+    
     def authorize():
         """
         - Use Authorization Code flow to generate an access token and refresh token
         - Once access token expires, use refresh token to generate a new token
         """
-        endpoint= "/authorize"
-        
-        payload = {
+        params = {
             'response_type':    "code",
             'client_id':        self.client_id,
             'redirect_uri':     self.redirect_uri
         }
         
-        res = requests.get(BASE + ENDPOINT, params=payload)
+        res = self._get("/authorize", params=params)
     
     def get_token():
         endpoint = "/api/token"
+        
+        if path.exists(self.cache_file):
+            with open(CACHE_FILE, 'r') as json_file:
+                data = json.load(json_file)
+                access_token = data['access_token']
+                refresh_token = data['refresh_token']
+                expiration_time = data['expiration_time']
+
+    def load_from_cache():
+        data = {}
+        with open(self.cache_file, 'r') as json_file:
+            data = json.load(json_file)
+        return data
+    
+    @staticmethod
+    def save_to_cache(data):
+        with open(CACHE_FILE, 'w') as json_file:
+            json.dumps(data, json_file)
+            
         
     def refresh_token():
         pass
--- test.py
+++ test.py
@@ -9,18 +9,18 @@ import spotipy.util as util
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-
+    username = input("Username: ")
 
 
 scope = "playlist-modify-public user-top-read"
 token = util.prompt_for_user_token(username, scope)
 
 sp = spotipy.Spotify(auth=token)
-tracks = sp.current_user_top_tracks(time_range="short_term", limit=50)["items"]
-playlist_id = sp.user_playlist_create(username, "test")["id"]
+#sp.trace = True
 
-uris = [track["uri"] for track in tracks]
+track_objs = sp.current_user_top_tracks(time_range="short_term", limit=50)["items"]
+tracks = [track["uri"] for track in track_objs]
+
+playlist_id = sp.user_playlist_create(username, "test")["id"]
 
 sp.user_playlist_add_tracks(username, playlist_id, tracks)
